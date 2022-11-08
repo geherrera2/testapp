b2d1ab71e55c9930fbda010b101f9603ec34c0b8
German Herrera <germaneherrera@gmail.com>
2022-11-08 17:29:02 -0500

    fix: elimina codigo duplicado

--- src/app/modules/fincas/components/crear-finca-modal/crear-finca-modal.component.ts
+++ src/app/modules/fincas/components/crear-finca-modal/crear-finca-modal.component.ts
@@ -10,6 +10,7 @@ import { AlertService } from '../../../shared/services/alert/alert.service';
 import { ParametricasModel } from '../../../shared/models/parametricas.model';
 import { Router } from '@angular/router';
 import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+import { GpsService } from '@shared/services/gps/gps.service';
 
 @Component({
   selector: 'app-crear-finca-modal',
@@ -29,8 +30,7 @@ export class CrearFincaModalComponent implements OnInit {
     public parametricasService: ParametricasService,
     private fincasService: FincasService,
     private alertService: AlertService,
-    private geolocation: Geolocation,
-    private androidPermissions: AndroidPermissions
+    private gpsService:GpsService
   ) { }
 
   ngOnInit() {
@@ -121,51 +121,7 @@ export class CrearFincaModalComponent implements OnInit {
   }
 
   getGps(){
-    this.androidPermissions.checkPermission(
-          this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION)
-      .then( data => {
-        if ( data.hasPermission ){
-          this.getUbicacion();
-        } else {
-          this.androidPermissions.requestPermissions(
-            [
-              this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION,
-              this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION
-            ]
-            ).then( respGps => {
-              if ( respGps.hasPermission ){
-                this.getUbicacion();
-              } else {
-                this.errorGps();
-              }
-            });
-        }
-      }, err => {
-        this.errorGps(err);
-      }
-    );
-  }
-
-  private errorGps(error?: any) {
-    this.alertService.activarLoading(false);
-    let msg = '';
-    if (error?.code === 2) {
-      msg = 'la aplicación no tiene suficientes permisos de geolocalización';
-    } else {
-      msg = 'No fue posible capturar la geolocalización';
-    }
-    this.formulario.controls.ubication.setValue(`--`);
-    this.alertService.presentAlert(msg, ['Aceptar']);
-  }
-
-  private getUbicacion() {
-    this.alertService.activarLoading(true);
-    this.geolocation.getCurrentPosition().then((resp) => {
-      this.alertService.activarLoading(false);
-      this.formulario.controls.ubication.setValue(`${resp.coords.latitude}, ${resp.coords.longitude}`);
-    }).catch((error) => {
-      this.errorGps(error);
-    });
+    this.gpsService.getGps(this.formulario)
   }
 
 }
