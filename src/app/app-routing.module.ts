43fa650eb177d2a37819e6a808e85bedd371a48c
Manuel Ortiz Villamarin <m.ortizv2@uniandes.edu.co>
2022-11-09 18:06:47 -0500

    Fix some issues reported by SonarQube

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
-import { SessionGuard } from './guards/session.guard';
 
 const routes: Routes = [
   {
